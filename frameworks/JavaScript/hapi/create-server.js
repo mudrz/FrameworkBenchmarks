1053367913a288181968bdd0841288686c6c8848
mudrz <mudrz@outlook.com>
2020-10-08 16:30:49 +0300

    wip

--- frameworks/JavaScript/hapi/create-server.js
+++ frameworks/JavaScript/hapi/create-server.js
@@ -3,54 +3,55 @@
  * working implementation for the benchmark suite.
  */
 
-const Hapi = require('hapi');
-const Vision = require('vision');
-
-const options = {
-  connections: {
-    compression: false
-  }
-};
-
-const server = new Hapi.Server(options);
-
-server.connection({port: process.env.PORT || 8080, host: '0.0.0.0'});
-server.register(Vision, (err) => {
-    if (err) {
-        throw err;
-    }
-
-    server.views({
-        engines: { html: require('handlebars') },
-        path: __dirname + '/views/'
-    });
-});
+const Hapi = require('@hapi/hapi');
 
 const Handler = require(`./handlers/${process.env.NODE_HANDLER}`);
 
-// Makes routing simpler as tfb routes are all GET's
-// We also don't use the nifty route features that Hapi has
-// to offer such as attaching a validator
-const Route = (path, handler) =>
-  server.route({ method: 'GET', path, handler });
-
 const JsonSerialization = (req, reply) =>
-  reply({ message: 'Hello, World!' }).header('Server', 'hapi');
+  reply
+    .response({ message: 'Hello, World!' })
+    .header('Server', 'hapi');
 
 const Plaintext = (req, reply) =>
-  reply('Hello, World!')
+  reply
+    .response('Hello, World!')
     .header('Server', 'hapi')
     .header('Content-Type', 'text/plain');
 
+const start = async () => {
+    const server = Hapi.server({
+        port: process.env.PORT || 8080,
+        host: '0.0.0.0',
+        compression: false,
+    });
+    await server.register(require('@hapi/vision'));
+    server.views({
+        engines: {
+            html: require('handlebars')
+        },
+        path: __dirname + '/views/'
+    });
 
-Route('/json', JsonSerialization);
-Route('/plaintext', Plaintext);
+    // Makes routing simpler as tfb routes are all GET's
+    // We also don't use the nifty route features that Hapi has
+    // to offer such as attaching a validator
+    const Route = (path, handler) =>
+      server.route({ method: 'GET', path, handler });
 
-Route('/db', Handler.SingleQuery);
-Route('/queries', Handler.MultipleQueries);
-Route('/fortunes', Handler.Fortunes);
-Route('/updates', Handler.Updates);
+    Route('/json', JsonSerialization);
+    Route('/plaintext', Plaintext);
 
-server.start((err) =>
-  console.log('Hapi worker started and listening on ' + server.info.uri + " "
-    + new Date().toISOString(" ")));
+    Route('/db', Handler.SingleQuery);
+    Route('/queries', Handler.MultipleQueries);
+    Route('/fortunes', Handler.Fortunes);
+    Route('/updates', Handler.Updates);
+
+    await server.start();
+    console.log('Hapi worker started and listening on ' + server.info.uri + " " + new Date().toISOString(" "));
+}
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+start();
